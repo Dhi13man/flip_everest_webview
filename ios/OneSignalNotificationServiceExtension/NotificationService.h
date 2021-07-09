ded1b48ed97cde7976eb580c6e860dae1e710c57
Dhiman Seal <dhi13man@mac13book.local>
2021-07-09 03:33:53 -0700

    ios set up init

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Dhiman Seal on 09/07/21.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
